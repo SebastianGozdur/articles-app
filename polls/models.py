187694a0f81b406d87e47a042a6de3f24d3bead5
gozdur.sebastian <gozdur.sebastian@gmail.com>
2018-01-24 16:25:38 +0100

    fixed in layout

--- polls/models.py
+++ polls/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class Article(models.Model):
     articleName = models.CharField(max_length=100)
     rating = models.IntegerField()
+    author = models.CharField(max_length=100)
+    description = models.TextField()
 
     def __str__(self):
         return self.articleName
